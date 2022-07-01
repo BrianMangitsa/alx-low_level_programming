2376cdece9730a09df6d6fa09feb423758a9ecbd
brian mangitsa <brianmangitsa1@gmail.com>
2022-07-01 08:20:21 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *  * main - Print a string using put
- *   *
- *    * Return: "Programming is like building a multilingual puzzle
- *     */
+ * main - Print a string using put
+ *
+ *Return: "Programming is like building a multilingual puzzle
+*/
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
