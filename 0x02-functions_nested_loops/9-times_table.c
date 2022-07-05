583d7a55017c38f3ab2ce4410cbe70d9e5c0e1fe
brian mangitsa <brianmangitsa1@gmail.com>
2022-07-05 13:44:20 -0700

    my first commit;

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,20 +14,21 @@ void times_table(void)
 	{
 		for ( ; d <= 9; d++)
 		{
-																	if (((n * d) / 10) != 0)
-																		_putchar(((n * d) / 10) + '0');
-																	else if ((n * d) % 10 != 0)
-																		_putchar(' ');
-																	else if (n == 0 && d > 0)
-																		_putchar(' ');
-																	_putchar((n * d) % 10 + '0');
-																															if (d < 9)
-																															{
-																																_putchar(',');
-																																_putchar(' ');
-																																													}
-																														}
-			_putchar('\n');
-			d = 0;
-														}
+			if (((n * d) / 10) != 0)
+				_putchar(((n * d) / 10) + '0');
+			else if ((n * d) % 10 != 0)
+				_putchar(' ');
+			else if (n == 0 && d > 0)
+				_putchar(' ');
+			_putchar((n * d) % 10 + '0');
+			if (d < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		d = 0;
+	}
+
 }
