568ee53bb0de10db63698dc285a3441bfda9a85d
brian mangitsa <brianmangitsa1@gmail.com>
2022-07-05 09:46:17 -0700

    my first commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* jack_bauer  Entry point to receive integers.
+* jack_bauer - Entry point to receive integers.
 *
 * Description: prints every minute of the day of Jack Bauer.
-* starting from 00:00 to 23:59.
 * Return: void
 */
 void jack_bauer(void)
@@ -19,6 +18,6 @@ void jack_bauer(void)
 																	_putchar('0' + (m / 10));
 																	_putchar('0' + (m % 10));
 																	_putchar('\n');
-																}
+																														}
 	}
 }
