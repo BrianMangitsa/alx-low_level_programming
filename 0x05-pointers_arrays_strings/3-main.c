df4b4a621b6d271fc54c58796128a6e3a581610a
brian mangitsa <brianmangitsa1@gmail.com>
2022-07-11 05:15:03 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "My first strlen!";
+	_puts(str);
+	return (0);
+}
